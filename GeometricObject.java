4f1e41d6d4bbe6d6a62682a2e372141de183e57a
tcuc <infotcuc@gmail.com>
2017-10-22 01:03:26 +0000

    added a couple lines. just some variables and stuff.

--- GeometricObject.java
+++ GeometricObject.java
@@ -1,5 +1,32 @@
-class GeometricObject
+import System.MATH;
+
+public abstract class GeometricObject
 {
+	private int numberOfObjects = 0;
+	private int length;
+	private int hight;
+	private int radius;
+	
+	float findArea()
+	{
+		return length*hight;
+	}
+	
+	float findVolume()
+	{
+		
+	}
+	
+	float findCircumference()
+	{
+		return radius*MATH.PI;
+	}
+	
+	float findSurfaceArea()
+	{
+		
+	}
+	
 	public static void main(String[] args)
 	{
 	    
