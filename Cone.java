51201dd25473e36b940de5f813348425ea4a9a34
TC <infotcuc@gmail.com>
2017-11-02 15:27:22 +0100

    moved cone comments to a new Cone class

---
+++ Cone.java
@@ -0,0 +1,27 @@
+public abstract class Cone extends Circle
+{
+
+		//##
+		// How to find inner radius
+		//##
+		// a = tan−1( H/R )
+		// d =cos( a )* T
+		// r= R - d 
+		// r is the innervolumes radius. 
+		// H is the height
+		// R is the radius
+		// T is the thickness
+		// (all capital variables are known values)
+
+	//##
+	// Getters and setters
+	//##
+
+	//##
+	// Methods
+	//##
+	
+	//###
+	// Constructors
+	//###
+}
