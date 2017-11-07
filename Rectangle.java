3ca7497d3d0eeb4fe1f1e2382f7596f1a0341b54
TC <infotcuc@gmail.com>
2017-11-07 09:27:36 +0100

    removed abstractmethods(these are not needed in this class), and reorganized code.

--- Rectangle.java
+++ Rectangle.java
@@ -3,49 +3,26 @@ public abstract class Rectangle extends GeometricObject
 	private double length;
 	private double width;
 
-	abstract double findVolume();
-	abstract double findSurfaceArea();
-	abstract double findWeight();
-
 	//##
 	// Getters and setters
 	//##
-	public double getLength() {
+	public double getLength() 
+	{
 		return length;
 	}
-	public void setLength(double length) {
+	public void setLength(double length) 
+	{
 		this.length = length;
 	}
-	public double getWidth() {
+	public double getWidth() 
+	{
 		return width;
 	}
-	public void setWidth(double width) {
+	public void setWidth(double width) 
+	{
 		this.width = width;
 	}
-
-	//##
-	// Methods
-	//##
-	double findArea() {
-		return length*width;
-	}
-	double findCircumference()
-	{
-		return 2*length + 2*width;
-	}
-
-	@Override
-	public String toString() {
-		String enStreng;
-		enStreng = super.toString() + "\n" +
-					"Lengde:" + getLength() + "\n" +
-					"Bredde:" + getWidth()  + "\n" +
-					"Areal:" + findArea() + "\n" +
-					"Omkrets:" + findCircumference();
-		return enStreng;
-	}
-
-	
+		
 	//###
 	// Constructors
 	//###
@@ -57,5 +34,29 @@ public abstract class Rectangle extends GeometricObject
 	Rectangle()
 	{
 		this(1, 1);
+	}
+
+	//##
+	// Methods
+	//##
+	double findArea() 
+	{
+		return length*width;
+	}
+	double findCircumference()
+	{
+		return 2*length + 2*width;
+	}
+
+	@Override
+	public String toString() 
+	{
+		String enStreng;
+		enStreng = super.toString() + "\n" +
+					"Lengde:" + getLength() + "\n" +
+					"Bredde:" + getWidth()  + "\n" +
+					"Areal:" + findArea() + "\n" +
+					"Omkrets:" + findCircumference();
+		return enStreng;
 	}
 }
